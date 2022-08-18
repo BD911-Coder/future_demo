c13fc343e907037b880027c438fe6236986f8caa
BD911-Coder <fc.developer@icloud.com>
2022-08-18 15:48:02 +0300

    Future builde added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
 void main(List<String> args) {
-  
+  print('Before the future');
+
+  final myFuture = Future<int>.delayed(
+    Duration(milliseconds: 1000),
+    () => 42,
+  )
+      .then(
+        (value) => print('Value : $value'),
+      )
+      .catchError(
+        (onError) => print('Error : $onError'),
+      )
+      .whenComplete(
+        () => print('Future is completed'),
+      );
+  print('After the future');
 }

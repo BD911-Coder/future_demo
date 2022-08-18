930082b82065b1881bf8cf032cc13a3091ca0caf
BD911-Coder <fc.developer@icloud.com>
2022-08-18 18:29:58 +0300

    main.dart updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
-void main(List<String> args) {
-  print('Before the future');
-  try {
-    final value = Future<int>.delayed(
-      Duration(seconds: 1),
-      () => 42,
-    );
-    throw Exception('An Error!');
-    print('Value: $value');
-  } catch (error) {
-    print(error);
-  } finally {
-    print('Future is complete');
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+import 'assets/todo.dart';
+
+Future<void> main() async {
+  final url = 'https://jsonplaceholder.typicode.com/todos/1';
+  final parsedUrl = Uri.parse(url);
+  final response = await http.get(parsedUrl);
+  final statusCode = response.statusCode;
+  if (statusCode == 200) {
+    final rawJsonString = response.body;
+    final jsonMap = jsonDecode(rawJsonString);
+    final todo = Todo.fromJson(jsonMap);
+    print(todo);
+  } else {
+    throw HttpException('$statusCode');
   }
-  print('After the future');
 }
